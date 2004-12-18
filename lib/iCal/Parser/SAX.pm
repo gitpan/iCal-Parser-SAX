f1f832dfcd37d1d02d684d639945120e31ea04ec
Rick Frankel <rfrankel@cpan.org>
2004-12-18 21:30:05 +0000

    Import of RFRANKEL/iCal-Parser-SAX-1.02 from CPAN.
    gitpan-cpan-distribution: iCal-Parser-SAX
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         RFRANKEL/iCal-Parser-SAX-1.02.tar.gz
    gitpan-cpan-author:       RFRANKEL
    gitpan-cpan-maturity:     released

--- lib/iCal/Parser/SAX.pm
+++ lib/iCal/Parser/SAX.pm
@@ -1,4 +1,4 @@
-#$Id: SAX.pm,v 1.1 2004/12/18 01:03:51 rick Exp $
+#$Id: SAX.pm,v 1.2 2004/12/18 20:36:36 rick Exp $
 package iCal::Parser::SAX;
 use strict;
 
@@ -9,7 +9,7 @@ use IO::File;
 use IO::String;
 use DateTime;
 
-our $VERSION=sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+our $VERSION=sprintf("%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/);
 our @EXPORT= qw();
 our @EXPORT_OK= qw();
 
@@ -396,4 +396,4 @@ LICENSE file included with this module.
 
 =head1 SEE ALSO
 
-L<Text:iCal::asData>, L<XML::SAX::Base>
+L<iCal::Parser>, L<XML::SAX::Base>
